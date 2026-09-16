c604eea7ee9bdaa4d1ffc9eb6ea44b27c13b8a24
damonSkappel <dskappel@cord.edu>
2026-09-16 15:06:22 -0500

    Externalize database credentials using dotenv

--- src/main/java/com/damonskappel/flighttracker/FlighttrackerApplication.java
+++ src/main/java/com/damonskappel/flighttracker/FlighttrackerApplication.java
@@ -1,5 +1,6 @@
 package com.damonskappel.flighttracker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlighttrackerApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(FlighttrackerApplication.class, args);
-
     }
-
 }
