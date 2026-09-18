27ec45c55d5379c3652a4a64ae7f613c27056b44
damonSkappel <dskappel@cord.edu>
2026-09-18 16:06:46 -0500

    Add custom domain to CORS allowed in origins

--- src/main/java/com/damonskappel/flighttracker/config/WebConfig.java
+++ src/main/java/com/damonskappel/flighttracker/config/WebConfig.java
@@ -8,8 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/flights/**").allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080", "null").allowedMethods("GET").allowedHeaders("*").maxAge(3600);
+        registry.addMapping("/flights/**").allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080", "null").allowedMethods("GET").allowedHeaders("*").maxAge(3600).allowedOrigins(
+                "http://localhost:8080",
+                "http://127.0.0.1:8080",
+                "null",
+                "https://flighttracker.damonskappel.com"
+        );
 
-        registry.addMapping("/stats").allowedOrigins("http://localhost:8080", "http:127.0.0.1:8080", "null").allowedMethods("GET").allowedHeaders("*").maxAge(3600);
+        registry.addMapping("/stats").allowedOrigins("http://localhost:8080", "http:127.0.0.1:8080", "null").allowedMethods("GET").allowedHeaders("*").maxAge(3600).allowedOrigins(
+                "http://localhost:8080",
+                "http://127.0.0.1:8080",
+                "null",
+                "https://flighttracker.damonskappel.com"
+        );
+
+
     }
 }
